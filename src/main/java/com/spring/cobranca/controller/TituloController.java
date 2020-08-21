57f006b7b10ebe7a8b83698a0a089cdd3273a313
Lucas <lucas.teixeira34@gmail.com>
2020-08-20 22:04:05 -0300

    Cadastro de títulos

--- src/main/java/com/spring/cobranca/controller/TituloController.java
+++ src/main/java/com/spring/cobranca/controller/TituloController.java
@@ -1,13 +1,28 @@
 package com.spring.cobranca.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.spring.cobranca.model.Titulo;
+import com.spring.cobranca.repository.TituloRepository;
 
 @Controller
+@RequestMapping("/titulos")
 public class TituloController {
 	
-	@RequestMapping("/titulos/novo")
+	@Autowired
+	private TituloRepository tituloRepository;
+	
+	@RequestMapping("/novo")
 	public String novo() {
+		return "CadastroTitulo";
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String salvar(Titulo titulo) {
+		System.out.println(">>> " + titulo.getDescricao());
 		return "CadastroTitulo";
 	}
 
