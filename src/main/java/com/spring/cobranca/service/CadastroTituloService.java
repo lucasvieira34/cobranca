839bbd44b4a5df03bd54d963e6a266feb9dc6526
Lucas <lucas.teixeira34@gmail.com>
2020-08-22 17:07:43 -0300

    Implementação de Ajax no Status

--- src/main/java/com/spring/cobranca/service/CadastroTituloService.java
+++ src/main/java/com/spring/cobranca/service/CadastroTituloService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import com.spring.cobranca.model.StatusTitulo;
 import com.spring.cobranca.model.Titulo;
 import com.spring.cobranca.repository.TituloRepository;
 
@@ -23,6 +24,14 @@ public class CadastroTituloService {
 
 	public void excluir(Long codigo) {
 		tituloRepository.deleteById(codigo);
+	}
+
+	public String receber(Long codigo) {
+		Titulo titulo = tituloRepository.findById(codigo).get();
+		titulo.setStatus(StatusTitulo.RECEBIDO);
+		tituloRepository.save(titulo);
+		
+		return StatusTitulo.RECEBIDO.getDescricao();
 	}
 
 }
